a7f4aa05a9226817a63aebd53734e8b59c59c73c
joseph <joseph@edmondscommerce.co.uk>
2015-02-12 13:09:45 +0000

    properly resolving symlinks now

--- bin/selenium-run.bash
+++ bin/selenium-run.bash
@@ -1,6 +1,17 @@
 #!/bin/bash
-DIR="$( cd -p "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 cd $DIR/../binaries/
+if [[ $? != 0 ]]
+then
+    echo "Failed cd-ing into the the binaries folder, aborting"
+    exit 1
+fi
 MAJOR_VERSION=2.44
 VERSION=${MAJOR_VERSION}.0
 JAR_FILE=selenium-server-standalone-${VERSION}.jar
